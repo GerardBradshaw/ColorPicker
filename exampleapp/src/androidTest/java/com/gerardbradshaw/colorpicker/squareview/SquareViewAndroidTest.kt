b5178d931c34c2d60fd79c6b3c4e57a934dd59ea
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2020-09-25 17:38:01 -0700

    Added basic test for Square view

---
+++ exampleapp/src/androidTest/java/com/gerardbradshaw/colorpicker/squareview/SquareViewAndroidTest.kt
@@ -0,0 +1,56 @@
+package com.gerardbradshaw.colorpicker.squareview
+
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.gerardbradshaw.colorpicker.InputParams
+import com.gerardbradshaw.colorpicker.OutputColors
+import com.gerardbradshaw.colorpicker.R
+import com.gerardbradshaw.colorpicker.SquareViewActivity
+import com.gerardbradshaw.colorpicker.util.AndroidTestUtil.checkPreviewChangedColorTo
+import com.gerardbradshaw.colorpicker.util.AndroidTestUtil.moveSeekBarTo
+import com.gerardbradshaw.colorpicker.squareview.SquareViewAndroidTestUtil.moveThumbTo
+import com.gerardbradshaw.colorpicker.util.AndroidTestUtil.getParamaterizedTestParams
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class SquareViewAndroidTest(private val input: InputParams, private val expected: OutputColors) {
+
+  @Rule
+  @JvmField
+  val asr = ActivityScenarioRule<SquareViewActivity>(SquareViewActivity::class.java)
+
+
+
+  // ---------------- PREVIEW TESTS ----------------
+
+  @Test
+  fun should_updatePreview_when_colorSliderProgressChanged() {
+    moveSeekBarTo(input.colorProgress, R.id.slider_seek_bar)
+    checkPreviewChangedColorTo(expected.pureColor, R.id.large_preview_new)
+  }
+
+  @Test
+  fun should_updatePreview_when_colorAndShadeProgressChanged() {
+    moveSeekBarTo(input.colorProgress, R.id.slider_seek_bar)
+    moveThumbTo(input.tintRatio, input.shadeRatio, R.id.large_color_window)
+    checkPreviewChangedColorTo(expected.shadedColor, R.id.large_preview_new)
+  }
+
+
+
+  // ---------------- UTIL ----------------
+
+  companion object {
+    private const val TAG = "GGG"
+    private const val sliderMax = 16777216
+
+    @Parameterized.Parameters
+    @JvmStatic
+    fun data(): Collection<Array<Any>> {
+      return getParamaterizedTestParams()
+    }
+  }
+}
---
+++ exampleapp/src/androidTest/java/com/gerardbradshaw/colorpicker/squareview/SquareViewAndroidTestUtil.kt
@@ -0,0 +1,42 @@
+package com.gerardbradshaw.colorpicker.squareview
+
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.MotionEvents
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+
+
+object SquareViewAndroidTestUtil {
+  fun moveThumbTo(xRatio: Double, yRatio: Double, squareResId: Int) {
+    onView(allOf(withId(squareResId), isDisplayed()))
+      .perform(touchSquareDownAndUp(xRatio, yRatio))
+  }
+
+  fun touchSquareDownAndUp(xRatio: Double, yRatio: Double): ViewAction? {
+    return object : ViewAction {
+      override fun getConstraints(): Matcher<View> {
+        return isDisplayed()
+      }
+
+      override fun getDescription(): String {
+        return "Send touch events."
+      }
+
+      override fun perform(uiController: UiController, view: View) {
+        val touchX = (view.width * xRatio).toFloat()
+        val touchY = (view.height * yRatio).toFloat()
+
+        val touchCoordinates = floatArrayOf(touchX, touchY)
+        val precision = floatArrayOf(1f, 1f)
+
+        val down = MotionEvents.sendDown(uiController, touchCoordinates, precision).down
+        uiController.loopMainThreadForAtLeast(200)
+        MotionEvents.sendUp(uiController, down, touchCoordinates)
+      }
+    }
+  }
+}
